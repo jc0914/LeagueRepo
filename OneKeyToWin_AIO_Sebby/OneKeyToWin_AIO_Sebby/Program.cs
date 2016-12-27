629fa548a4d11f6d745654207ac73121dc363256
OneKeyToWin <kaczor.sebastian@gmail.com>
2016-12-27 20:22:30 +0100

    news

--- OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Program.cs
+++ OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Program.cs
@@ -24,7 +24,7 @@ namespace OneKeyToWin_AIO_Sebby
 
         public static Spell Q, W, E, R, Q1, W1, E1, R1;
 
-        private static string OktNews = "Prediction collision fixed, prediction improve";
+        private static string OktNews = "TRACKER rework";
 
         public static AioMode AioModeSet = AioMode.All;
         public static Menu Config;
