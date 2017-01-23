527c586f46f58fe03a1a5ef9e93ee2f2c84f59ed
jc0914 <jc20000914@gmail.com>
2017-01-24 02:38:17 +0800

    Add files via upload

---
+++ 花边自用系列最终测试版/花边自用多合一V2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace 花边自用多合一
+{
+    using System;
+    using LeagueSharp.Common;
+    using myUtility;
+
+    internal class Program
+    {
+        private static void Main(string[] Args)
+        {
+            CustomEvents.Game.OnGameLoad += OnLoad;
+        }
+
+        private static void OnLoad(EventArgs Args)
+        {
+            myAllInOne.Init();
+        }
+    }
+}
---
+++ 花边自用系列最终测试版/花边自用英雄系列试用版本/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace 花边自用英雄系列试用版本
+{
+    using LeagueSharp;
+    using LeagueSharp.Common;
+    using myChampionsAIO_Test;
+
+    internal class Program
+    {
+        private static void Main(string[] Args)
+        {
+            CustomEvents.Game.OnGameLoad += eventArgs =>
+            {
+                if (!string.IsNullOrEmpty(ObjectManager.Player.ChampionName))
+                {
+                    myChampionInject.作者QQ("1076751236");
+                }
+            };
+        }
+    }
+}
