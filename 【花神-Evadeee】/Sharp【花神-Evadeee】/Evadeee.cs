bcc577bd44f8f5ca31dd7e75cb4db86ec474aac0
jc0914 <jc20000914@gmail.com>
2017-01-24 00:42:43 +0800

    Add files via upload

---
+++ 【花神-Evadeee】/Sharp【花神-Evadeee】/Evadeee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Evadeee
+{
+    class Program
+    {
+        private static Evade evade;
+        
+        static void Main(string[] args)
+        {
+            evade = new Evade();
+        }
+    }
+}
---
+++ 【花神-Evadeee】/Sharp【花神-Evadeee】/SpecialSpells/Darius.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace Evadeee.SpecialSpells
+{
+    class Darius : ChampionPlugin
+    {
+        static Darius()
+        {
+            // todo: fix for multiple darius' on same team (one for all)
+        }
+
+        public void LoadSpecialSpell(SpellData spellData)
+        {
+            if (spellData.spellName == "DariusCleave")
+            {
+                Game.OnUpdate += Game_OnUpdate;
+                SpellDetector.OnProcessSpecialSpell += SpellDetector_OnProcessSpecialSpell;
+            }
+        }
+
+        private void Game_OnUpdate(EventArgs args)
+        {
+            var darius = HeroManager.AllHeroes.FirstOrDefault(x => x.ChampionName == "Darius");
+            if (darius != null && darius.CheckTeam())
+            {
+                foreach (var spell in SpellDetector.detectedSpells.Where(x => x.Value.heroID == darius.NetworkId))
+                {
+                    if (spell.Value.info.spellName == "DariusCleave")
+                    {
+                        spell.Value.startPos = darius.ServerPosition.To2D();
+                        spell.Value.endPos = darius.ServerPosition.To2D() + spell.Value.direction * spell.Value.info.range;                   
+                    }
+                }
+            }
+        }
+
+        private void SpellDetector_OnProcessSpecialSpell(Obj_AI_Base hero, GameObjectProcessSpellCastEventArgs args, SpellData spellData, SpecialSpellEventArgs specialSpellArgs)
+        {
+            if (spellData.spellName == "DariusCleave")
+            {
+                //SpellDetector.CreateSpellData(hero, start.To3D(), end.To3D(), spellData);
+            }
+        }
+    }
+}
