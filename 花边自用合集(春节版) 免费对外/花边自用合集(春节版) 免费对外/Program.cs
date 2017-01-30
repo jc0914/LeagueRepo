5fefacf0c7674a22390836269ac596bd451053e0
jc0914 <jc20000914@gmail.com>
2017-01-31 01:52:28 +0800

    Add files via upload

---
+++ 花边自用合集(春节版) 免费对外/花边自用合集(春节版) 免费对外/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace 花边自用英雄系列
+{
+    using LeagueSharp;
+    using LeagueSharp.Common;
+    using myChampionsAIO;
+
+    internal class Program
+    {
+        private static void Main(string[] Args)
+        {
+            CustomEvents.Game.OnGameLoad += eventArgs =>
+            {
+                if (!string.IsNullOrEmpty(ObjectManager.Player.ChampionName))
+                {
+                    myChampionInject.LoadUserID("1076751236");
+                }
+            };
+        }
+    }
+}
